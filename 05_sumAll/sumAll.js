07b151de98dee7a68f4dcf3551727be2d9074989
bobmeix <bobmeix@yahoo.com>
2022-07-31 23:17:33 +0300

    Pass 6 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (numStart, numEnd) {
+    if (numStart < 0
+        || numEnd < 0
+        || !Number.isInteger(numStart)
+        || !Number.isInteger(numEnd)) {
+        return 'ERROR'
+    }
+    let sum = 0;
+    for (let i = Math.min(numStart, numEnd);
+        i <= Math.max(numStart, numEnd); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
