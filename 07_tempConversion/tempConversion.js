b11cc40f2e14bb9286b7894d1f65b449c0066f09
bobmeix <bobmeix@yahoo.com>
2022-07-31 23:48:07 +0300

    Pass 6 tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  return parseFloat(((tempF - 32) / 1.8).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  return parseFloat(((tempC * 1.8) + 32).toFixed(1));
 };
 
 // Do not edit below this line
