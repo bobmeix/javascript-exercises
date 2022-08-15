7694d251b0c366fcc79564b2414611d441b256cb
bobmeix <bobmeix@yahoo.com>
2022-08-15 18:07:23 +0300

    Pass 3 tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    let oldest = people.sort((a, b) => 
+    getAge(b.yearOfBirth, b.yearOfDeath) - getAge(a.yearOfBirth,  a.yearOfDeath))[0];
+    return oldest;
+};
 
+const getAge = function(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
