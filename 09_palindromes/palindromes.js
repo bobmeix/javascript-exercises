09c5a79745c52f9aed9ab569e4b1cbc8284e755c
bobmeix <bobmeix@yahoo.com>
2022-08-15 14:04:37 +0300

    Pass 6 tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let punctuation = ' ,.!';
+    let filteredString = str
+        .split('')
+        .filter(letter => punctuation.indexOf(letter) === -1)
+        .join('')    
+        .toLowerCase();
+    
+    let reversedString = filteredString
+        .split('')
+        .reverse()
+        .join('')
+    for (let i = 0; i < filteredString.length; i++) {
+        if (filteredString[i] !== reversedString[i]) {
+            return false;
+        }
+    } return true;
 };
 
 // Do not edit below this line
